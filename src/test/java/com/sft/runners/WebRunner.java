c6f7361dba663bc89cb4e86f0b2b09fef32480b3
Siva <>
2025-08-12 07:21:00 +0530

    chaintest partially done

--- src/test/java/com/sft/runners/WebRunner.java
+++ src/test/java/com/sft/runners/WebRunner.java
@@ -8,14 +8,14 @@ import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"com/sft/steps","com/sft/hooks"},
-         plugin = {
-                "pretty",
+        glue = {"com/sft/steps", "com/sft/hooks"},
+        plugin = {
+                //"com.aventstack.chaintest.plugins.ChainTestCucumberListener:",
                 "html:target/cucumber-html-reports",
                 "html:target/cucumber-report.html",
                 "html:target/cucumber-reports/cucumber-pretty",
                 "json:target/cucumber-reports/CucumberTestReport.json",
-                "timeline:target/test-output-thread/",
+                "timeline:target/test-output-thread/"
         },
         //tags = "@Smoke or @Regression and @Bug"
         tags = "@Smoke"
@@ -24,12 +24,12 @@ public class WebRunner extends AbstractTestNGCucumberTests {
 
     @DataProvider(parallel = true)
     @Override
-    public Object[][] scenarios(){
+    public Object[][] scenarios() {
         return super.scenarios();
     }
 
     @BeforeClass
-    public void beforeClass(ITestContext iTestContext){
+    public void beforeClass(ITestContext iTestContext) {
         //iTestContext.getCurrentXmlTest().getSuite().setDataProviderThreadCount(2);
     }
 }
